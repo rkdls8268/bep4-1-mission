cb0a48e6328c2114851e671dfb45dc1f27c08da4
rkdls8268 <rkdls8268@gmail.com>
2025-12-30 17:49:17 +0900

    0050 - 배치 잡을 스케쥴러에 의해서 매일 01시, 04시, 22시에 수행

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -3,9 +3,11 @@ package com.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing // 테이블안에 row data 가 생길 때 자동으로 생성, 수정 관련 데이터가 쌓임
+@EnableScheduling
 public class BackApplication {
 
     public static void main(String[] args) {
